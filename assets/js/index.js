dca9519884b6efc607a95a6f0176288d400aaafb
Harry <35490839+AbsentMinded1@users.noreply.github.com>
2018-05-21 00:11:34 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,14 +13,15 @@ var quotations = [
 	{ quote: "If music be the food of love, play on", spoken_by: "William Shakespeare" },
 	{ quote: "The way to get started is to quit talking and begin doing", spoken_by: "Walt Disney" },
 	{ quote: "Obstacles are those frightful things you see when you take your eyes off the goal", spoken_by: "Henry Ford" },
-	{ quote: "I skate where the puck is going to be, not where it has been", spoken_by: "Wayne Gretzky" },
+	{ quote: "I'm always amazed that people take what I say seriously. I don't even take what I am seriously.", spoken_by: "David Bowie" },
 	{ quote: "When you come to a fork in the road, take it", spoken_by: "Yogi Berra" },
 	{ quote: "We may affirm absolutely that nothing great in the world has been accomplished without passion", spoken_by: "Hegel" },
 	{ quote: "The life which is unexamined is not worth living", spoken_by: "Socrates" },
 	{ quote: "Live as if you were to die tomorrow. Learn as if you were to live forever", spoken_by: "M.K. Gandhi" },
-	{ quote: "What you get by achieving your goals is not as important as what you become by achieving your goals", spoken_by: "Zig Ziglar" },
-	{ quote: "Indeed", spoken_by: "GriefMoDz 2k15 ;)" },
-	{ quote: "I've ran out of quotes to put here... 😞", spoken_by: "" }
+	{ quote: "I'm just a musical prostitute, my dear.", spoken_by: "Freddy Mercury" },
+	{ quote: "Indeed", spoken_by: "GriefMoDz <3" },
+	{ quote: "I've ran out of quotes to put here... 😞", spoken_by: "" },
+	{ quote: "Work until your bank account looks like a phone number.", spoken_by: "Leonardo DiCaprio" }
 ]
 
 var randomNum = Math.floor(Math.random() * quotations.length);
